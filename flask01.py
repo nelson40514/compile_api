31a5c1aa000cb4e497141a98d5efb5ef5f3cebca
NelsonSu <33085066+nelson40514@users.noreply.github.com>
2020-12-13 00:21:24 +0800

    Update flask01.py

--- flask01.py
+++ flask01.py
@@ -10,5 +10,4 @@ def hello_world():
 def test_page():
   return 'In test page!'
 
-if __name__ == '__main__':
-  app.run(threaded=True,host='0.0.0.0', port=80)
+app.run(threaded=True,host='0.0.0.0', port=80)
