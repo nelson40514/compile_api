b2fb956e0f1c1f698c60e07c93a1b031caa7d723
= <=>
2020-12-14 00:34:16 +0800

    update api

--- flask01.py
+++ flask01.py
@@ -1,14 +1,50 @@
 from flask import *
 
 app = Flask(__name__)
+app.config["DEBUG"] = True
+#Define config mode
 
+# test data
+tpe = {
+    "id": 0,
+    "city_name": "Taipei",
+    "country_name": "Taiwan",
+    "is_capital": True,
+    "location": {
+        "longitude": 121.569649,
+        "latitude": 25.036786
+    }
+}
+nyc = {
+    "id": 1,
+    "city_name": "New York",
+    "country_name": "United States",
+    "is_capital": False,
+    "location": {
+        "longitude": -74.004364,
+        "latitude": 40.710405
+    }
+}
+ldn = {
+    "id": 2,
+    "city_name": "London",
+    "country_name": "United Kingdom",
+    "is_capital": True,
+    "location": {
+        "longitude": -0.114089,
+        "latitude": 51.507497
+    }
+}
+cities = [tpe, nyc, ldn]
 
-@app.route('/')
+@app.route('/',methods=['GET'])
 @app.route('/index')
 def index():
-    data = "Deploying a Flask App To Heroku"
-    data += "<br>Test01"
+    data = "<h1>Hello Flask api</h1>"
     return data
 
+@app.route('/cities/all',methods=['GET'])
+def cities_all():
+    return jsonify(cities)
 if __name__ == '__main__':
     app.run(debug=True)
