02627cc1a46cfa3035e2bf5631a0d723d945c34b
= <=>
2020-12-14 00:45:43 +0800

    update api

--- flask01.py
+++ flask01.py
@@ -3,12 +3,14 @@ from flask import *
 app = Flask(__name__)
 app.config["DEBUG"] = True
 #Define config mode
+app.config['JSON_AS_ASCII'] = False
+#show 繁體中文
 
 # test data
 tpe = {
     "id": 0,
-    "city_name": "Taipei",
-    "country_name": "Taiwan",
+    "city_name": "臺北",
+    "country_name": "台灣",
     "is_capital": True,
     "location": {
         "longitude": 121.569649,
@@ -17,8 +19,8 @@ tpe = {
 }
 nyc = {
     "id": 1,
-    "city_name": "New York",
-    "country_name": "United States",
+    "city_name": "紐約",
+    "country_name": "美國",
     "is_capital": False,
     "location": {
         "longitude": -74.004364,
@@ -27,8 +29,8 @@ nyc = {
 }
 ldn = {
     "id": 2,
-    "city_name": "London",
-    "country_name": "United Kingdom",
+    "city_name": "倫敦",
+    "country_name": "英國",
     "is_capital": True,
     "location": {
         "longitude": -0.114089,
@@ -46,5 +48,27 @@ def index():
 @app.route('/cities/all',methods=['GET'])
 def cities_all():
     return jsonify(cities)
+
+@app.route('/cities',methods=['GET'])
+def city_name():
+    if 'city_name' in request.args:
+        city_name = request.args['city_name']
+    else:
+        return "Error: No city_name provided. Please specify a city_name."
+    results = []
+
+    for city in cities:
+        if city['city_name'] == city_name:
+            results.append(city)
+
+    return jsonify(results)
+
+
+
+
+
+
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
